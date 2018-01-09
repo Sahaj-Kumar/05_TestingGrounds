8f51fb9e4601ebc94e9b41f911a5b7ebfe37ac0e
Sahaj-Kumar <sahaj.kumar@gmail.com>
2018-01-09 17:46:51 -0500

    Configuring the Gun

--- Source/S05_TestingGrounds/Player/FirstPersonCharacter.cpp
+++ Source/S05_TestingGrounds/Player/FirstPersonCharacter.cpp
@@ -87,6 +87,10 @@ void AFirstPersonCharacter::BeginPlay()
 
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+
+	// Bind fire event
+	InputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
 
 	// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
 	/*
@@ -115,8 +119,7 @@ void AFirstPersonCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	// Bind fire event
-	//PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFirstPersonCharacter::OnFire);
+	// NOTE: Setting fire event moved to begin play after setting up gun
 
 	// Enable touchscreen input
 	EnableTouchscreenMovement(PlayerInputComponent);
