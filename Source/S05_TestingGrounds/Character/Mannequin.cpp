449551d2ea3d4973776fbed13c9bfbd966282a21
Sahaj-Kumar <sahaj.kumar@gmail.com>
2018-01-21 19:04:02 -0500

    Converting a character BP to C++

---
+++ Source/S05_TestingGrounds/Character/Mannequin.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mannequin.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "../Weapons/Gun.h"
+
+// Sets default values
+AMannequin::AMannequin()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// camera setup
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>("FirstPersonCamera");
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->RelativeLocation = FVector(-39.56f, 1.65f, 64.0f);
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// FP arms mesh
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	Mesh1P->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
+
+}
+
+// Called when the game starts or when spawned
+void AMannequin::BeginPlay()
+{
+	Super::BeginPlay();
+	if (!GunBlueprint) {
+		// gun blueprint is missing
+		return;
+	}
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+	//InputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
+
+}
+
+// Called every frame
+void AMannequin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AMannequin::Fire()
+{
+	Gun->OnFire();
+}
+
